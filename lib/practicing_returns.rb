33b43ffcc5d08c695d0f073d2ce8b8c627bfd7c8
Amos Fatoma <amosfatoma@gmail.com>
2020-11-23 04:36:51 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,12 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
+    binding.pry
     collection << yield(array[i])
     i += 1
   end
   collection
-  binding.pry
 end
 
+ruby
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
